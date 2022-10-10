342b1bcbbbfc3bd7728f77dc17584343e1d8fb1a
Anna Zdyb <anna.zdyb@gmail.com>
2022-10-10 14:50:31 +0200

    learning about Observables, and asynch operations

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Hero } from './heroes/hero.model';
 import { HeroesList } from './heroes/listOfHeroes';
+import { Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root' //with his declattion is available app wide
@@ -10,7 +11,8 @@ export class HeroService {
   constructor() { }
 
 
-  getHeroes(): Hero[] {
-    return HeroesList;
+  getHeroes(): Observable<Hero[]> {
+    const asynchHeroesList = of(HeroesList);
+    return asynchHeroesList;
   }
 }
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,57 +1,61 @@
 import { Component, OnInit } from '@angular/core';
-import { Hero } from './hero.model'
+import { Hero } from './hero.model';
 // import { HeroesList } from './listOfHeroes';
 import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.css']
+  styleUrls: ['./heroes.component.css'],
 })
 export class HeroesComponent implements OnInit {
-// chosenHero:Hero;
-// heroes = HeroesList; //redundant after adding service
-heroes: Hero[] = [];
-chosenHero?: Hero;
+  // chosenHero:Hero;
+  // heroes = HeroesList; //redundant after adding service
+  heroes: Hero[] = [];
+  chosenHero?: Hero;
 
-  constructor(private heroService: HeroService) { }
+  constructor(private heroService: HeroService) {}
 
   ngOnInit(): void {
     console.log('on Init oif Heroes');
-    console.log(this.heroes  )
+    console.log(this.heroes);
 
     //from the service
-    this.heroes = this.heroService.getHeroes();
+    // this.heroes = this.heroService.getHeroes()
+//------------the asynch HTTP method Observable------------//
+      this.heroService.getHeroes()
+        .subscribe( (stillHeroes) => this.heroes = stillHeroes)
+    
+    // This approach won't work in a real application that uses asynchronous calls.
+    // It works now because your service synchronously returns mock heroes.
   }
 
   // getHeroes(): void {
   //   this.heroes = this.heroService.getHeroes();
   // }
 
-  onSelectHero(selectHero: Hero){
-    this.chosenHero = selectHero
-    console.log(selectHero.name)
+  onSelectHero(selectHero: Hero) {
+    this.chosenHero = selectHero;
+    console.log(selectHero.name);
   }
-//adding heroes List from services
- 
-
+  //adding heroes List from services
 }
 
 // heroes: Hero = [
 //   {id: 1,
 //   name: ' Sir Gregory ',
 //   power: ' fighter ⚔️',
-//   }, 
+//   },
 //   {id: 2,
 //     name: ' Ice Queen ',
 //     power: ' freeze ❄️',
-//     }, 
+//     },
 //   {id: 3,
 //     name: ' Voltan ',
 //     power: ' electricity ⚡',
-//     }, 
+//     },
 //   {id: 4,
 //       name: ' Eon ',
 //       power: ' time stop ⏳',
-//       }, 
+//       },
 // ]
