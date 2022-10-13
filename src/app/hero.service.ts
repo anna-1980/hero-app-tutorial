84caaeb1f8693b23a605a18332d09d3c18fb0236
Anna Zdyb <anna.zdyb@gmail.com>
2022-10-13 16:35:46 +0200

    mock edit hero name , add, delete hero

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -8,70 +8,105 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { catchError, map, tap } from 'rxjs/operators';
 
 @Injectable({
-  providedIn: 'root' //with his declattion is available app wide
+  providedIn: 'root', //with his declattion is available app wide
 })
 export class HeroService {
-
   constructor(
     private http: HttpClient,
-    private messageService: MessageService,) { }
+    private messageService: MessageService
+  ) {}
 
-  private log(message: string){
+  private log(message: string) {
     this.messageService.add(`HeroService: ${message}`);
-    }
+  }
 
-  private heroesUrl = 'api/heroes';  // URL to web api
+  private heroesUrl = 'api/heroes'; // URL to web api
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
+  };
 
-  getHeroes(): Observable<Hero[]> {
-    // const asynchHeroesList = of(HeroesList);
-    // this.messageService.add('HeroService: fetched asynchHeroesList');
-    // return asynchHeroesList;
-    return this.http.get<Hero[]>(this.heroesUrl)
-    .pipe(
-      tap(_ => this.log('fetched heroes')),
-      catchError(this.handleError<Hero[]>('getHeroes', []))
+  /** PUT: update the hero on the server */
+  updateHero(hero: Hero): Observable<any> {
+    return this.http.put(this.heroesUrl, hero, this.httpOptions).pipe(
+      tap((_) => this.log(`updated hero id=${hero.id}`)),
+      catchError(this.handleError<any>('updateHero'))
     );
   }
 
-/**
- * Handle Http operation that failed.
- * Let the app continue.
- *
- * @param operation - name of the operation that failed
- * @param result - optional value to return as the observable result
- */
- private handleError<T>(operation = 'operation', result?: T) {
-  return (error: any): Observable<T> => {
+  /** POST: add a new hero to the server */
+  addHero(hero: Hero): Observable<Hero> {
+    return this.http.post<Hero>(this.heroesUrl, hero, this.httpOptions).pipe(
+      tap((newHero: Hero) => this.log(`added hero w/ id=${newHero.id}`)),
+      catchError(this.handleError<Hero>('addHero'))
+    );
+  }
 
-    // TODO: send the error to remote logging infrastructure
-    console.error(error); // log to console instead
+  getHeroes(): Observable<Hero[]> {
+    const asynchHeroesList = of(HeroesList);
+    this.messageService.add('HeroService: fetched asynchHeroesList');
+    return asynchHeroesList;
 
-    // TODO: better job of transforming error for user consumption
-    this.log(`${operation} failed: ${error.message}`);
+    //SERVER
+    // return this.http.get<Hero[]>(this.heroesUrl)
+    // .pipe(
+    //   tap(_ => this.log('fetched heroes')),
+    //   catchError(this.handleError<Hero[]>('getHeroes', []))
+    // );
+  }
 
-    // Let the app keep running by returning an empty result.
-    return of(result as T);
-  };
-}
+  /**
+   * Handle Http operation that failed.
+   * Let the app continue.
+   *
+   * @param operation - name of the operation that failed
+   * @param result - optional value to return as the observable result
+   */
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
 
-getHero(id: number, heroName?: string | null): Observable<Hero> {
-  const url = `${this.heroesUrl}/${id}`;
-  return this.http.get<Hero>(url).pipe(
-    tap(_ => this.log(`fetched hero id=${id}`)),
-    catchError(this.handleError<Hero>(`getHero id=${id}`))
-  );
-}
+      // TODO: better job of transforming error for user consumption
+      this.log(`${operation} failed: ${error.message}`);
 
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
+  }
+
+  // SERVER
   // getHero(id: number, heroName?: string | null): Observable<Hero> {
-  //   const syncHero = HeroesList.find(h => h.id === id)!;
-  //   this.messageService.add(` HeroService: fetched Single Hero ${id} "${heroName}" ${new Date().toString().slice(0, 21)}` ) 
-  //   return of(syncHero); //as observable
+  //   const url = `${this.heroesUrl}/${id}`;
+  //   return this.http.get<Hero>(url).pipe(
+  //     tap(_ => this.log(`fetched hero id=${id}`)),
+  //     catchError(this.handleError<Hero>(`getHero id=${id}`))
+  //   );
   // }
+
+  getHero(id: number, heroName?: string | null): Observable<Hero> {
+    const syncHero = HeroesList.find((h) => h.id === id)!;
+    this.messageService.add(
+      ` HeroService: fetched Single Hero ${id} "${heroName}" ${new Date()
+        .toString()
+        .slice(0, 21)}`
+    );
+    return of(syncHero); //as observable
+  }
 
   getHerobyName(heroName?: string | null): Observable<Hero> {
     // const syncHeroname = HeroesList.find(n => n.name === n)!;
-    const singleHeroName = HeroesList.find(n => n.name === heroName)!;
+    const singleHeroName = HeroesList.find((n) => n.name === heroName)!;
     return of(singleHeroName); //as observable
+  }
+
+  /** DELETE: delete the hero from the server */
+  deleteHero(id: number): Observable<Hero> {
+    const url = `${this.heroesUrl}/${id}`;
+
+    return this.http.delete<Hero>(url, this.httpOptions).pipe(
+      tap((_) => this.log(`deleted hero id=${id}`)),
+      catchError(this.handleError<Hero>('deleteHero'))
+    );
   }
 }
 
--- src/app/heroes/listOfHeroes.ts
+++ src/app/heroes/listOfHeroes.ts
@@ -1,21 +1,13 @@
-import { Hero } from "./hero.model";
+import { Hero } from './hero.model';
 
 //remember to add the TYPE to Hero !
 export const HeroesList: Hero[] = [
-    {id: 1,
-    name: ' Sir Gregory ',
-    power: ' fighter ⚔️',
-    }, 
-    {id: 2,
-      name: ' Ice Queen ',
-      power: ' freeze ❄️',
-      }, 
-    {id: 3,
-      name: ' Voltan ',
-      power: ' electricity ⚡',
-      }, 
-    {id: 4,
-        name: ' Eon ',
-        power: ' time stop ⏳',
-        }, 
-  ]
+  { id: 1, name: ' Sir Gregory ', power: ' fighter ⚔️' },
+  { id: 2, name: ' Ice Queen ', power: ' freeze ❄️' },
+  { id: 3, name: ' Voltan ', power: ' electricity ⚡' },
+  { id: 4, name: ' Dr. Stretch ', power: ' elasticity 🌀' },
+  { id: 4, name: ' Optiko ', power: ' Light bending 🌈' },
+  { id: 4, name: ' Archiey ', power: ' aim 🏹' },
+  { id: 4, name: ' Dragoness ', power: ' shape shifter 🐉' },
+  { id: 4, name: ' Blasta ', power: ' tele kinetic explosion 💥' },
+];
